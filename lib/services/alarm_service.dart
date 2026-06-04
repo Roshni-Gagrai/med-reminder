e3fa30e5acc367913ed538a9d45ef70dd9bc590a
Roshni Gagrai <roshnigagrai00@gmail.com>
2026-06-04 17:12:38 +0530

    alarm service changed

--- lib/services/alarm_service.dart
+++ lib/services/alarm_service.dart
@@ -2,7 +2,6 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
 
 @pragma('vm:entry-point')
 void notificationTapBackground(NotificationResponse response) {}
@@ -10,7 +9,7 @@ void notificationTapBackground(NotificationResponse response) {}
 class AlarmService {
   static final FlutterLocalNotificationsPlugin _notifications =
       FlutterLocalNotificationsPlugin();
-  static final AudioPlayer _player = AudioPlayer();
+
   static bool _initialized = false;
 
   static Future<void> initialize(
@@ -35,9 +34,7 @@ class AlarmService {
 
     await _notifications.initialize(
       initSettings,
-      onDidReceiveNotificationResponse: (response) {
-        _showAlarmDialog(navigatorKey);
-      },
+      onDidReceiveNotificationResponse: (response) {},
       onDidReceiveBackgroundNotificationResponse: notificationTapBackground,
     );
 
@@ -51,96 +48,7 @@ class AlarmService {
             AndroidFlutterLocalNotificationsPlugin>()
         ?.requestExactAlarmsPermission();
 
-    final launchDetails =
-        await _notifications.getNotificationAppLaunchDetails();
-    if (launchDetails?.didNotificationLaunchApp == true) {
-      _showAlarmDialog(navigatorKey);
-    }
-
     _initialized = true;
-  }
-
-  static void _showAlarmDialog(GlobalKey<NavigatorState> navigatorKey) async {
-    await _player.setReleaseMode(ReleaseMode.loop);
-    await _player.play(AssetSource('sounds/alarm.mp3'));
-
-    navigatorKey.currentContext != null
-        ? showDialog(
-            context: navigatorKey.currentContext!,
-            barrierDismissible: false,
-            builder: (_) => AlertDialog(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              backgroundColor: const Color(0xFF1A0A2E),
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.purple.shade700.withOpacity(0.3),
-                      border: Border.all(
-                          color: Colors.purple.shade300, width: 2),
-                    ),
-                    child: const Icon(
-                      Icons.medication_rounded,
-                      size: 40,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  const Text(
-                    'Time to Take\nYour Medicine',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      height: 1.4,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    TimeOfDay.now().format(navigatorKey.currentContext!),
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.purple.shade200,
-                    ),
-                  ),
-                  const SizedBox(height: 24),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        await _player.stop();
-                        navigatorKey.currentState?.pop();
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.purple.shade600,
-                        padding:
-                            const EdgeInsets.symmetric(vertical: 14),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                      child: const Text(
-                        'TAKEN',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 2,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          )
-        : null;
   }
 
   static Future<void> scheduleDailyAlarm({
